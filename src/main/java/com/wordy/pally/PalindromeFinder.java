86b7363de20ada3e2850ada78c25d3f4b0de2c1e
gawain <gawain.hammond@gmail.com>
2017-12-07 21:35:34 +0000

    added repeat character method

--- src/main/java/com/wordy/pally/PalindromeFinder.java
+++ src/main/java/com/wordy/pally/PalindromeFinder.java
@@ -11,7 +11,25 @@ import java.util.List;
  *
  */
 public class PalindromeFinder {
+
     public List<Palindrome> find(String fyddycent, int i) {
         return new ArrayList<Palindrome>();
+    }
+
+
+
+    /**
+     * Use while parsing string or character arrays to flag second repeating letter in a given string
+     *
+     * @param characters given string to find repeating letters
+     * @param atIndex index of second repeating character
+     * @return true if given index is repeating character
+     */
+    public boolean isRepeatCharacterAtIndex(char[] characters, int atIndex) {
+        if (atIndex < 1 || atIndex > characters.length -1) return false;
+        char currentCharacter = characters[atIndex];
+        char previousCharacter = characters[atIndex -1];
+        if (currentCharacter == previousCharacter) return true;
+        return false;
     }
 }
--- src/test/java/com/wordy/pally/PalindromeWordSearchTest.java
+++ src/test/java/com/wordy/pally/PalindromeWordSearchTest.java
@@ -15,4 +15,36 @@ public class PalindromeWordSearchTest {
         List<Palindrome> results  = palindromeFinder.find("fyddycent", 1);
         assertNotNull(results);
     }
+
+    @Test
+    public void testIsRepeatCharacterAtIndex1() {
+        String inputString = "abbawabba";
+        PalindromeFinder palindromeFinder = new PalindromeFinder();
+        boolean isRepeat = palindromeFinder.isRepeatCharacterAtIndex(inputString.toCharArray(), 1);
+        assertFalse(isRepeat);
+    }
+
+    @Test
+    public void testIsRepeatCharacterAtIndex2() {
+        String inputString = "abbawabba";
+        PalindromeFinder palindromeFinder = new PalindromeFinder();
+        boolean isRepeat = palindromeFinder.isRepeatCharacterAtIndex(inputString.toCharArray(), 2);
+        assertTrue(isRepeat);
+    }
+
+    @Test
+    public void testIsRepeatCharacterOutOfBounds() {
+        String inputString = "abbawabba";
+        PalindromeFinder palindromeFinder = new PalindromeFinder();
+        
+        boolean isRepeat = palindromeFinder.isRepeatCharacterAtIndex(inputString.toCharArray(), 64);
+        assertFalse(isRepeat);
+
+        isRepeat = palindromeFinder.isRepeatCharacterAtIndex(inputString.toCharArray(), -1);
+        assertFalse(isRepeat);
+
+        isRepeat = palindromeFinder.isRepeatCharacterAtIndex(inputString.toCharArray(), 0);
+        assertFalse(isRepeat);
+    }
+
 }
